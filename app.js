3b9c47c78e70202a22f459ea2b0a4a61575758f4
Steven Miller <steven.miller114@gmail.com>
2021-04-21 17:05:24 -0700

    added changes to html and js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const settings = {
 };
 
 function displayWinLoss(responseJson) {
-    $('#wins').replaceWith(
+    $('#wins').empty();
+    $('#wins').append(
         `<li>Wins: ${responseJson.br.wins}</li>
         <li>Times in the Top 5: ${responseJson.br.topFive}</li>
         <li>Times in the Top 10: ${responseJson.br.topTen}</li>
@@ -24,7 +25,8 @@ function displayWinLoss(responseJson) {
 }
 
 function displayKillDeath(responseJson) {
-    $('#kills').replaceWith(
+    $('#kills').empty();
+    $('#kills').append(
         `<li>Kills: ${responseJson.br.kills}</li>
         <li>Deaths: ${responseJson.br.deaths}</li>
         <li>K/D Ratio: ${responseJson.br.kdRatio}</li>`
@@ -32,7 +34,8 @@ function displayKillDeath(responseJson) {
 }
 
 function displayScore(responseJson) {
-    $('#scores').replaceWith(
+    $('#scores').empty();
+    $('#scores').append(
         `<li>Score: ${responseJson.br.score}</li>
         <li>Time Played: ${responseJson.br.timePlayed}</li>
         <li>Score per Minute: ${responseJson.br.scorePerMinute}</li>`
@@ -46,6 +49,7 @@ function displayResults(responseJson) {
     $('#win-loss').removeClass('hidden');
     $('#kill-death').removeClass('hidden');
     $('#score').removeClass('hidden');
+    $('#js-error-message').empty();
     $('#profile-name').val('');
     $('#js-select-menu').val('');
 }
@@ -64,7 +68,25 @@ function findProfile(userProfile, userPlatform) {
         })
         .then(responseJson => displayResults(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            $('#win-loss').addClass('hidden');
+            $('#kill-death').addClass('hidden');
+            $('#score').addClass('hidden');
+            $('#profile-name').val('');
+            $('#js-select-menu').val('');
+            $('#js-error-message').html(`<h3>There was a problem locating your profile: ${err.message}.</h3>
+            <p>If you have issues accessing the information on your account make sure to do the following:</p>
+            <ul>
+              <li>Make sure to replace the "#" in your profile name with "%23". Example: profile#1111 = profile%231111</li>
+              <li>Your account may have privacy options set to prevent your stats from being pulled.
+                <ol>
+                  <li>Visit <a href="https://s.activision.com/activision/login">Activision Login</a></li>
+                  <li>Head to your "Profile"</li>
+                  <li>Under "Account Linking" find your platform</li>
+                  <li>Change the "Searchable" and "Data Visible" settings to "All"</li>
+                </ol>
+              </li>
+              <li>There are occasionally issues with finding your profile using Activision ID. You could try another platform if it doesn't work.</li>
+            </ol>`);
         });
 }
 
